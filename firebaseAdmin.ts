ae2fcef12fd22ab171b07a667f356353a10698d2
UnRealReincarlution <benwhite2413@gmail.com>
2021-03-04 16:42:32 +1300

    Private Key

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,10 +1,10 @@
 import * as firebaseAdmin from "firebase-admin";
 
-const { privateKey } = JSON.parse(process.env["FIREBASE_PRIVATE_KEY"]);
+const { pk } = JSON.parse(process.env["PK"]);
 const clientEmail = `firebase-adminsdk-28q7g@learn-to-code-nz.iam.gserviceaccount.com`;
 const projectId = "learn-to-code-nz";
 
-if (!privateKey || !clientEmail || !projectId) {
+if (!pk || !clientEmail || !projectId) {
   console.log(
     `Failed to load Firebase credentials. Follow the instructions in the README to set your Firebase credentials inside environment variables.`
   );
@@ -13,7 +13,7 @@ if (!privateKey || !clientEmail || !projectId) {
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: privateKey,
+      privateKey: pk,
       clientEmail,
       projectId,
     }),
